1774a277435494c2309b283ea3c3534a98472edb
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-01-10 08:29:39 +0600

    Variables and Strings all problems solved

---
+++ Variables and Strings/trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Sazid";
+const botLocation = "Dhaka";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "" + favoriteLanguage + " was created in only 10 days.";
+console.log(codingFact);
+
+codingFact = "" + favoriteLanguage + " is not related to Java.";
+console.log(codingFact);
+
+codingFact = "" + favoriteLanguage + " can be run outside the browser.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
