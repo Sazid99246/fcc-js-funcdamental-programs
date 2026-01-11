a0e45355ad2969636b1e544eaf5766527f8a66c5
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-01-11 10:08:46 +0600

    Chunky Monky algorithm lab completed

---
+++ Loops/chunky-monky-algorithm.js
@@ -0,0 +1,19 @@
+function chunkArrayInGroups(arr, n) {
+    let result = [];
+    let firstIndex = 0;
+    let secondIndex = n;
+    while (firstIndex < arr.length) {
+        result.push(arr.slice(firstIndex, secondIndex));
+        firstIndex += n;
+        secondIndex += n;
+    }
+    return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
